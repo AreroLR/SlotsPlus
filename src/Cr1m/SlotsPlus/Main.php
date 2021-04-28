53e156e1a4a3cf2320dffe473930133c541fe90c
Ben <xzbennzx@gmail.com>
2021-04-28 01:21:38 -0700

    Update Main.php

--- src/Cr1m/SlotsPlus/Main.php
+++ src/Cr1m/SlotsPlus/Main.php
@@ -52,12 +52,13 @@ class Main extends PluginBase implements Listener
             $event->setPlayerCount($fakeCount);
         }
     }
+
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
         switch ($command->getName()) {
             case "fakeplayers":
-                if($sender->hasPermission("SlotsPlus.command.fakeplayers")) {
-                    if(isset($args[0])) {
+                if ($sender->hasPermission("SlotsPlus.command.fakeplayers")) {
+                    if (isset($args[0])) {
                         if (is_numeric($args[0])) {
                             $this->config->set("fakePlayers", true);
                             $this->config->set("fakeSlots", (int)$args[0]);
@@ -68,17 +69,15 @@ class Main extends PluginBase implements Listener
                             }
                             $this->config->save();
                             break;
-                        }
-                        else{
+                        } else {
                             $sender->sendMessage("Please only use an integer i.e. 7");
                             break;
                         }
 
-                    }
-                    else if ($this->config->get("fakePlayers") === true) {
+                    } else if ($this->config->get("fakePlayers") === true) {
                         $this->config->set("fakePlayers", false);
                         $sender->sendMessage("Disabled fake slots");
-                        if($this->config->get("unlimitedSlots") === true) {
+                        if ($this->config->get("unlimitedSlots") === true) {
                             $this->config->set("unlimitedSlots", false);
                         }
                         $this->config->save();
@@ -90,13 +89,13 @@ class Main extends PluginBase implements Listener
                 }
                 break;
             case "unlimitedslots":
-                if($sender->hasPermission("SlotsPlus.command.unlimitedslots")) {
+                if ($sender->hasPermission("SlotsPlus.command.unlimitedslots")) {
 
                     if ($this->config->get("unlimitedSlots") === false) {
 
                         $this->config->set("unlimitedSlots", true);
                         $sender->sendMessage("Enabled unlimited slots");
-                        if($this->config->get("fakePlayers") === true) {
+                        if ($this->config->get("fakePlayers") === true) {
                             $this->config->set("fakePlayers", false);
                         }
                         $this->config->save();
@@ -105,18 +104,17 @@ class Main extends PluginBase implements Listener
                     if ($this->config->get("unlimitedSlots") === true) {
                         $this->config->set("unlimitedSlots", false);
                         $sender->sendMessage("Disabled unlimited slots");
-                        if($this->config->get("fakePlayers") === true) {
+                        if ($this->config->get("fakePlayers") === true) {
                             $this->config->set("fakePlayers", false);
                         }
                         $this->config->save();
                         break;
                     }
-
-                    }
-                break;
-
                 }
-                return true;
+                break;
         }
+        return true;
+    }
+
 }
 
